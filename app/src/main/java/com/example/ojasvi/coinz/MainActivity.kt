dc91b385e141c613676f93e6f966d76befef89d0
Ojasvi-Jalal <s1612970@sms.ed.ac.uk>
2018-12-02 14:23:01 +0000

    netter login and registration

--- app/src/main/java/com/example/ojasvi/coinz/MainActivity.kt
+++ app/src/main/java/com/example/ojasvi/coinz/MainActivity.kt
@@ -127,7 +127,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationEngineList
                     var props = feature.properties()
                     var currency = props?.get("currency")
                     var currencyType = currency.toString()
-
                     var coinIcon = R.drawable.test
 //                    if(currencyType == "\"DOLR\"")
 //                        coinIcon = R.drawable.dolr
@@ -277,21 +276,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, LocationEngineList
             }
         }
     }
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        return when (item.itemId) {
+//            R.id.action_settings -> true
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
 
     public override fun onResume() {
         super.onResume()
--- app/src/main/java/com/example/ojasvi/coinz/SignUpActivity.kt
+++ app/src/main/java/com/example/ojasvi/coinz/SignUpActivity.kt
@@ -1,81 +1,54 @@
 package com.example.ojasvi.coinz
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.annotation.TargetApi
-import android.content.pm.PackageManager
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.app.LoaderManager.LoaderCallbacks
-import android.content.CursorLoader
-import android.content.Loader
-import android.database.Cursor
-import android.net.Uri
-import android.os.AsyncTask
-import android.os.Build
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.text.TextUtils
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.widget.ArrayAdapter
-import android.widget.TextView
-
-import java.util.ArrayList
-import android.Manifest.permission.READ_CONTACTS
-import android.app.ProgressDialog
 import android.content.Intent
+import android.text.TextUtils
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import com.google.firebase.auth.AuthResult
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-
-import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.toast
 
-/**
- * A login screen that offers login via email/password.
- */
-class LoginActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
+
 
     private var email_Text: EditText? = null
     private var password_Text: EditText? = null
+    private var username_Text: EditText? = null
     private var login_Button: Button? = null
-    private var signUpLink: TextView? = null
+    private var sign_Up_Button: TextView? = null
 
     private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
 
-        email_Text = findViewById<EditText>(R.id.username) as EditText
-        password_Text = findViewById<EditText>(R.id.password) as EditText
-        login_Button = findViewById<Button>(R.id.sign_in_button) as Button
-        signUpLink = findViewById(R.id.sign_up) as TextView
+        email_Text  = findViewById<EditText>(R.id.emailSignUp)
+        password_Text = findViewById<EditText>(R.id.passwordSignUp)
+        username_Text = findViewById(R.id.NickName)
+        login_Button = findViewById<Button>(R.id.login) as Button
+        sign_Up_Button = findViewById(R.id.register)
 
-        //Buttons
-        login_Button!!.setOnClickListener{login()}
+        sign_Up_Button!!.setOnClickListener(){signup()}
 
-        mAuth = FirebaseAuth.getInstance()
-
-        signUpLink!!.setOnClickListener {
-            //Start the register activity
-            Log.d(TAG,"Starting Registration")
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivityForResult(intent, REQUEST_SIGNUP)
+        login_Button!!.setOnClickListener(){
+            //Finish the registration screen and return to the Login activity
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
             finish()
         }
+
     }
 
-    fun login(){
-        Log.d("Message","Login")
+    fun signup(){
+        Log.d(TAG, "User Registration")
 
         login_Button!!.isEnabled = false
-
-        val progressDialog = ProgressDialog(this@LoginActivity,R.style.AppTheme)
 
         // Reset errors.
         email_Text?.error = null
@@ -84,11 +57,10 @@ class LoginActivity : AppCompatActivity() {
         // Store values at the time of the login attempt.
         val email = email_Text!!.text.toString()
         val password = password_Text!!.text.toString()
+        val nickname = username_Text!!.text.toString()
 
         var cancel = false
         var focusView: View? = null
-
-        Log.d(TAG, "In login activity, got email and password")
 
         // Check for a valid password, if the user entered one.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
@@ -114,16 +86,16 @@ class LoginActivity : AppCompatActivity() {
             focusView?.requestFocus()
         } else {
             Log.d("Message", "correct email and password were entered")
-            mAuth?.signInWithEmailAndPassword(email, password)
+
+            mAuth?.createUserWithEmailAndPassword(email, password)
                     ?.addOnCompleteListener(this) { task ->
                         if (task.isSuccessful) {
-                            Log.d(TAG, "Login Successful!")
-                            toast("Login Successful!")
-                            onLoginSuccess()
+                            Log.d(TAG, "Registration Successful!")
+                            toast("Registration Successful!")
                         } else {
                             //Sign in failed, display a message to the User
-                            Log.d(TAG, "Login Failed :( ${task.exception}")
-                            toast("Login Failed :(")
+                            Log.d(TAG, "Registration Failed :( ${task.exception}")
+                            toast("Registration Failed :(, Please Try Again!")
                         }
                     }
         }
@@ -140,19 +112,8 @@ class LoginActivity : AppCompatActivity() {
         return email.contains("@")
     }
 
-    fun onLoginSuccess(){
-        login_Button!!.isEnabled = true
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun onLoginFailed(){
-        toast("Login Failed :(")
-    }
-
     companion object {
-        private val TAG = "LoginActivity"
+        private val TAG = "SignUpActivity"
         private val REQUEST_SIGNUP = 0
     }
-
 }
